6f2554478512226d1892c8f048ad05f9485a2a38
kkochetkov05 <kkochetkov05@gmail.com>
2025-11-14 01:43:07 +0300

    довел до ума. можно парсить сразу много магазинов. ошибка 429 брутфорсится

--- WB_parser_429bruteforce/sources/brands.py
+++ WB_parser_429bruteforce/sources/brands.py
@@ -1,14 +1,9 @@
-brands = (
-    'gloria-jeans',
-    'befree',
-    'zarina',
-    'sela',
-    'ostin',
-    'tvoe',
-    '8168-colins',
-    'zolla',
-    '290923899-maag',
-    'yunichel',
-    'askona-46721',
-    '9752-omsa'
-)
+from brands_urls import brands_urls
+
+def extract_brand_names(brand_urls):
+    brands = set()
+    for url in brands_urls:
+        brands.add(url.split('/')[-1])
+    return brands
+
+brands = extract_brand_names(brands_urls)
