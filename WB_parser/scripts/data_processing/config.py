0219634c83778652384fc014d733ab51eb434874
kkochetkov05 <kkochetkov05@gmail.com>
2025-11-28 20:39:47 +0300

    вроде бы весь функционал. взаимодействие с программой происходит через файл scripts/main.py

--- WB_parser/scripts/data_processing/config.py
+++ WB_parser/scripts/data_processing/config.py
@@ -4,4 +4,5 @@ PROJECT_ROOT = Path(__file__).parent.parent.parent
 RAW_DATA_DIR = PROJECT_ROOT / "data" / "raw_temp_data"
 CLEAN_DATA_DIR = PROJECT_ROOT / "data" / "clean_temp_data"
 DB_PATH = PROJECT_ROOT / 'data' / 'sql_database' / 'wildberries_data.db'
+LIGHT_INDUSTRY_CATEGORIES_PATH = PROJECT_ROOT / 'sources' / 'light_industry_categories.json'
 table_name = "wildberries_data"
